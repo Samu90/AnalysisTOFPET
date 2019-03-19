82cceabf865adec9aa8f4863f0b7cdcba1c5ceb3
Samu90 <torelli.1654130@studenti.uniroma1.it>
2019-03-19 18:22:00 +0100

    Analysis temp1

--- AnalysisTemp.cpp
+++ AnalysisTemp.cpp
@@ -67,26 +67,30 @@ void Analysis(){
 
   
 
-
-
-
-
   TH1D* HistoCh59[(int)FileListPhysics.size()];
   TH1D* HistoCh315[(int)FileListPhysics.size()];
     
   Double_t MeanTCh59[(int)FileListPhysics.size()], MeanTCh315[(int)FileListPhysics.size()];
   Double_t SigmaTCh59[(int)FileListPhysics.size()], SigmaTCh315[(int)FileListPhysics.size()];
+  
+  Double_t Peak1Ch59[(int)FileListPhysics.size()], Peak2Ch59[(int)FileListPhysics.size()];
+  Double_t SigmaPeak1Ch59[(int)FileListPhysics.size()], SigmaPeak2Ch59[(int)FileListPhysics.size()];
 
+  Double_t Peak1Ch315[(int)FileListPhysics.size()], Peak2Ch315[(int)FileListPhysics.size()];
+  Double_t SigmaPeak1Ch315[(int)FileListPhysics.size()], SigmaPeak2Ch315[(int)FileListPhysics.size()];
   
 
+  TFile* f0;
+  TTree* tree0;
 
-
-
+  TF1* FitSpectrum[(int)FileListPhysics.size()][2];
+  
+  gStyle->SetOptFit(0111);
 
   for(int i=0;i < (int)FileListPhysics.size();i++){
     
-    TFile* f0= TFile::Open(("TestStability3/"+FileListPhysics.at(i)).c_str());
-    TTree* tree0 = (TTree*)f0->Get("data");
+    f0= TFile::Open(("TestStability3/"+FileListPhysics.at(i)).c_str());
+    tree0 = (TTree*)f0->Get("data");
     
     HistoCh59[i] = new TH1D(("HistoCh59N"+to_string(i)).c_str(),("HistoCh59N"+to_string(i)).c_str(),100,0,100);
     HistoCh315[i] = new TH1D(("HistoCh315N"+to_string(i)).c_str(),("HistoCh315N"+to_string(i)).c_str(),100,0,100);
@@ -96,18 +100,69 @@ void Analysis(){
     
     TCanvas* canvino = new TCanvas("Canvino","Canvino",1200,600);
     canvino->Divide(2,1);
+
+
+    FitSpectrum[i][0]=FitNaSpectrum(HistoCh59[i]);
+    FitSpectrum[i][1]=FitNaSpectrum(HistoCh315[i]);
     
     canvino->cd(1);
+    HistoCh59[i]->GetXaxis()->SetTitle("E [DU]");
+    HistoCh59[i]->GetYaxis()->SetTitle("Counts");
     HistoCh59[i]->Draw();
-    
+    FitSpectrum[i][0]->Draw("SAME");
+
     canvino->cd(2);
+    HistoCh315[i]->GetXaxis()->SetTitle("E [DU]");
+    HistoCh315[i]->GetYaxis()->SetTitle("Counts");
     HistoCh315[i]->Draw();
-      
+    FitSpectrum[i][1]->Draw("SAME");    
+
     canvino->SaveAs(("Plot/EnergyTempV2/Partials/Canvas"+to_string(i)+".png").c_str());
-    
     delete canvino;
   }
 
+  for(int i=0;i<(int)FileListPhysics.size();i++){
+    std::cout << MeanTCh59[i] << " " <<SigmaTCh59[i]<< " " << MeanTCh315[i] << " " << SigmaTCh315[i] << std::endl;
+
+    Peak1Ch59[i]=FitSpectrum[i][0]->GetParameter(1);
+    SigmaPeak1Ch59[i]=FitSpectrum[i][0]->GetParError(1);
+    Peak2Ch59[i]=FitSpectrum[i][0]->GetParameter(6);
+    SigmaPeak2Ch59[i]=FitSpectrum[i][0]->GetParError(6);
+
+    Peak1Ch315[i]=FitSpectrum[i][1]->GetParameter(1);
+    SigmaPeak1Ch315[i]=FitSpectrum[i][1]->GetParError(1);
+    Peak2Ch315[i]=FitSpectrum[i][1]->GetParameter(6);
+    SigmaPeak2Ch315[i]=FitSpectrum[i][1]->GetParError(6);
+    
+    std::cout << Peak1Ch59[i]<< " " << SigmaPeak1Ch59[i]<< " " << Peak2Ch59[i]<< " " << SigmaPeak2Ch59[i]<< " "<< Peak1Ch315[i]<< " " <<SigmaPeak1Ch315[i] <<" "<< Peak2Ch315[i]<< " " <<SigmaPeak2Ch315[i]<< std::endl; 
+    
+  }
+  
+  TCanvas* PlotEVsT = new TCanvas("PlotEVsT","PlotEVsT",1200,600);
+
+  TGraphErrors* Graph1Ch59 = new TGraphErrors((int)FileListPhysics.size(),MeanTCh59,Peak1Ch59,SigmaTCh59,SigmaPeak1Ch59);
+  TGraphErrors* Graph2Ch59 = new TGraphErrors((int)FileListPhysics.size(),MeanTCh59,Peak2Ch59,SigmaTCh59,SigmaPeak2Ch59);
+  
+  TGraphErrors* Graph1Ch315 = new TGraphErrors((int)FileListPhysics.size(),MeanTCh315,Peak1Ch315,SigmaTCh315,SigmaPeak1Ch315);
+  TGraphErrors* Graph2Ch315 = new TGraphErrors((int)FileListPhysics.size(),MeanTCh315,Peak2Ch315,SigmaTCh315,SigmaPeak2Ch315);
+
+  PlotEVsT->Divide(2,1);
+  PlotEVsT->cd(1);
+  Graph1Ch59->SetMaximum(90);
+  Graph1Ch59->SetMinimum(35);
+  Graph1Ch59->GetXaxis()->SetTitle("TMeanRun [°C]");
+  Graph1Ch59->GetYaxis()->SetTitle("E [DU]");
+  Graph1Ch59->Draw("AP");
+  Graph2Ch59->Draw("SAMEP");
+
+  PlotEVsT->cd(2);
+  Graph1Ch315->SetMaximum(90);
+  Graph1Ch315->SetMinimum(35);
+  Graph1Ch315->GetXaxis()->SetTitle("TMeanRun [°C]");
+  Graph1Ch315->GetYaxis()->SetTitle("E [DU]");
+  Graph1Ch315->Draw("AP");
+  Graph2Ch315->Draw("SAMEP");
 
 
+  PlotEVsT->SaveAs("Plot/EnergyTempV2/PlotEVsT.png");
 }
---
+++ FunctionsTempCB.cc
@@ -0,0 +1,134 @@
+void GetPedestal(TTree* tree,Double_t* pedch1,Double_t* pedch2){
+  
+  TH1F* histoCh1 = new TH1F("pippoch1","pippoch1",600,0,600);
+  TH1F* histoCh2 = new TH1F("pippoch2","pippoch2",600,0,600);
+
+  Float_t energy;
+  UShort_t chID;
+
+  tree->SetBranchAddress("energy",&energy);
+  tree->SetBranchAddress("channelID",&chID);
+   
+  for(int i=0; i<tree->GetEntries(); i++){
+    tree->GetEntry(i);
+    if(chID==59) { histoCh1->Fill(energy); }//chiudo if
+    else{ histoCh2->Fill(energy); }//chiudo else
+  }//chiudo for
+  
+  std::cout << "Filled"<< std::endl;
+
+  *pedch1 = histoCh1->GetMean();
+  *pedch2 = histoCh2->GetMean();
+  
+  delete histoCh1;
+  delete histoCh2;
+}
+
+
+void GetSpectrum(TTree* tree, TH1D* histoCh1, TH1D* histoCh2, Double_t MeanPedCh59, Double_t MeanPedCh315){
+  
+  Float_t energy;
+  UShort_t chID;
+
+  tree->SetBranchAddress("energy",&energy);
+  tree->SetBranchAddress("channelID",&chID);
+   
+  for(int i=0; i<tree->GetEntries(); i++){
+    tree->GetEntry(i);
+    if(chID==59) { histoCh1->Fill(energy-MeanPedCh59); }//chiudo if
+    else{ histoCh2->Fill(energy-MeanPedCh315); }//chiudo else
+  }//chiudo for
+  
+  std::cout << "Filled"<< std::endl;
+  
+  
+}
+
+
+
+void GetMeanTemperature(TTree* tree, Double_t* MeanTempCh59, Double_t* SigmaTempCh59,Double_t* MeanTempCh315, Double_t* SigmaTempCh315, Double_t* MeanTGlobal,Double_t* SigmaTGlobal){
+
+  UShort_t chID;
+  Double_t temp1,temp2,temp3;
+
+  TH1D* histo1 = new TH1D("pippoch1","pippoch1",20,20,40);
+  TH1D* histoCh59 = new TH1D("pippoch59","pippoch59",20,20,40);
+  TH1D* histoCh315 = new TH1D("pippoch315","pippoch315",20,20,40);
+
+  tree->SetBranchAddress("temp1",&temp1);
+  tree->SetBranchAddress("temp2",&temp2);
+  tree->SetBranchAddress("temp3",&temp3);
+  tree->SetBranchAddress("channelID",&chID);
+   
+  for(int i=0; i<tree->GetEntries(); i++){
+
+    tree->GetEntry(i);
+    histo1->Fill(temp1);
+    if(chID==59) { histoCh59->Fill(temp2); }//chiudo if
+    else{ histoCh315->Fill(temp3); }//chiudo else
+
+  }//chiudo for
+  
+  *MeanTGlobal= histo1->GetMean();
+  *SigmaTGlobal= histo1->GetMeanError();
+  *MeanTempCh59= histoCh59->GetMean();
+  *SigmaTempCh59= histoCh59->GetMeanError();
+  *MeanTempCh315= histoCh315->GetMean();
+  *SigmaTempCh315= histoCh315->GetMeanError();
+  
+  std::cout << "TMeanDone"<< std::endl;
+
+}
+
+
+TF1* FitNaSpectrum(TH1D* Profile){
+
+  TF1* spectrum = new TF1("SpectrumFit","[0] * exp(-( x-[1] )*( x-[1] )/( 2* [2]* [2])) + [3] / (exp( (x*[4]-(2*[1]*[1]/([1]+2*[1])))) + 1)+ [5] * exp(-( x-[6] )*( x-[6] )/( 2* [7]* [7])) + [8] / (exp( (x*[9]-(2*[6]*[6]/([1]+2*[6]))))+ 1)",30,90);
+  
+  spectrum->SetParameter(0,6000);
+  spectrum->SetParameter(1,40);
+  spectrum->SetParameter(2,3);
+  spectrum->SetParameter(3,1700);
+  spectrum->SetParameter(4,0.8);
+  spectrum->SetParameter(5,350);
+  spectrum->SetParameter(6,78);
+  spectrum->SetParameter(7,3.2);
+  spectrum->SetParameter(8,550);
+  spectrum->SetParameter(9,0.8);
+
+  spectrum->SetParLimits(5,250,500);
+  spectrum->SetParLimits(7,3,4);
+  spectrum->SetParLimits(8,300,700);
+  spectrum->SetParLimits(9,0.8,1);
+  
+
+
+
+  Profile->Fit("SpectrumFit","R0");
+
+  return spectrum;
+}
+
+TF1* FitNaSpectrumCB(TH1D* Profile){
+
+  
+  TF1* spectrum = new TF1("SpectrumFit","[0] * exp(-( x-[1] )*( x-[1] )/( 2* [2]* [2])) + [3] / (exp( (x*[4]-(2*[1]*[1]/([1]+2*[1])))) + 1)+ [5] * exp(-( x-[6] )*( x-[6] )/( 2* [7]* [7])) +crystalball([8],[9],[10],[11],[12])",30,88);
+  
+  spectrum->SetParameter(0,6000);
+  spectrum->SetParameter(1,41);
+  spectrum->SetParameter(2,3);
+  spectrum->SetParameter(3,1700);
+  spectrum->SetParameter(4,0.82);
+  spectrum->SetParameter(5,350);
+  spectrum->SetParameter(6,80);
+  spectrum->SetParameter(7,3.2);
+  spectrum->SetParameter(8,572);
+  spectrum->SetParameter(9,68);
+  spectrum->SetParameter(10,4.3);
+  spectrum->SetParameter(11,0.04);
+  spectrum->SetParameter(12,-1.4);
+
+  Profile->Fit("SpectrumFit","R0");
+
+  return spectrum;
+}
